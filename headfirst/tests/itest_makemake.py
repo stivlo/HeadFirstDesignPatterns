e10c631cbed60a2f45b39c020e9e1046bb47d727
Dan Buch <daniel.buch@gmail.com>
2009-11-07 16:28:08 -0500

    only asserting existence of project dirs once per dir

--- headfirst/tests/itest_makemake.py
+++ headfirst/tests/itest_makemake.py
@@ -5,6 +5,10 @@ from headfirst.tests._support import assert_exists
 
 def test_java_sources_classes_lister():
     lister = HM.JavaSourcesClassesLister(HEADFIRST_BASE)
+    dirpaths = []
     for dirpath, source_file, class_file in lister.get_sources_classes():
         yield assert_exists, lister.basedir, source_file
+        dirpaths.append(dirpath)
+
+    for dirpath in list(set(dirpaths)):
         yield assert_exists, dirpath
