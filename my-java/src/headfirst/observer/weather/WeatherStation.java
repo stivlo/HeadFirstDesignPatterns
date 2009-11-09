e8514e4c861d385bf0361746ebaa755819562953
Dan Buch <daniel.buch@gmail.com>
2009-11-09 17:26:08 -0500

    adding the heat index display, up through ch.2 p.61

--- my-java/src/headfirst/observer/weather/WeatherStation.java
+++ my-java/src/headfirst/observer/weather/WeatherStation.java
@@ -9,6 +9,7 @@ public class WeatherStation {
         StatisticsDisplay statisticsDisplay =
             new StatisticsDisplay(weatherData);
         ForecastDisplay forecastDisplay = new ForecastDisplay(weatherData);
+        HeatIndexDisplay heatIndexDisplay = new HeatIndexDisplay(weatherData);
 
         weatherData.setMeasurements(80, 65, 30.4f);
         weatherData.setMeasurements(82, 70, 29.2f);
