63bbe60038b0b6c15a6922ba821c31205a29dc59
Dan Buch <daniel.buch@gmail.com>
2010-03-26 20:20:10 -0400

    stubbing in life-changing app gui stuff

--- python/headfirst/observer/lifechangingapp.py
+++ python/headfirst/observer/lifechangingapp.py
@@ -1,7 +1,15 @@
 import sys
 
+import wx
+
 
 def main(sysargs=sys.argv[:]):
+    app = wx.App(False)
+
+    frame = wx.Frame(None, wx.ID_ANY, "Hello World")
+    frame.Show(True)
+
+    app.MainLoop()
     return 0
 
 
