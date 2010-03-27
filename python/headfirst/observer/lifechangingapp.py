9fda79f5b2d79152565c21620d5abf77a02c34c4
Dan Buch <daniel.buch@gmail.com>
2010-03-27 15:09:06 -0400

    cleaning up the gui observer example (python impl)

--- python/headfirst/observer/lifechangingapp.py
+++ python/headfirst/observer/lifechangingapp.py
@@ -4,22 +4,12 @@ import sys
 import Tkinter as tkinter
 
 
-class AngelListener(object):
-
-    def __init__(self, parent):
-        self.parent = parent
-
-    def action_performed(self, event):
-        print("Don't do it, you might regret it!")
+def angel_listener(event):
+    print("Don't do it, you might regret it!")
 
 
-class DevilListener(object):
-
-    def __init__(self, parent):
-        self.parent = parent
-
-    def action_performed(self, event):
-        print("Come on, do it!")
+def devil_listener(event):
+    print("Come on, do it!")
 
 
 class Application(tkinter.Frame):
@@ -34,19 +24,25 @@ class Application(tkinter.Frame):
         self.button = tkinter.Button(self)
         self.button.config(text="Should I do it?")
 
-        self.button.bind("<Button-1>", AngelListener(self).action_performed)
-        self.button.bind("<Button-1>", DevilListener(self).action_performed)
+        self.button.bind("<Button-1>", angel_listener)
+        self.button.bind("<ButtonRelease-1>", devil_listener)
 
         self.button.pack({"side": "left"})
 
 
 def main():
     root = tkinter.Tk()
+
     app = Application(master=root)
     app.master.title('Life-Changing Application')
     app.master.minsize(600, 600)
     app.master.maxsize(1000, 400)
     app.mainloop()
+
+    try:
+        root.destroy()
+    except tkinter.TclError:
+        pass
 
 
 if __name__ == '__main__':
