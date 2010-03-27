0a669b9faa68c96c73159c34eb7984e14eea399e
Dan Buch <daniel.buch@gmail.com>
2010-03-27 15:54:19 -0400

    adding condiment decorators for ch.3 p.97

---
+++ my-java/src/headfirst/decorator/starbuzz/SteamedMilk.java
@@ -0,0 +1,17 @@
+package headfirst.decorator.starbuzz;
+
+public class SteamedMilk extends CondimentDecorator {
+    Beverage beverage;
+
+    public SteamedMilk(Beverage beverage) {
+        this.beverage = beverage;
+    }
+
+    public String getDescription() {
+        return beverage.getDescription() + ", Steamed Milk";
+    }
+
+    public double cost() {
+        return 0.10 + beverage.cost();
+    }
+}
