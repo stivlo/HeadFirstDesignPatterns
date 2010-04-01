9066c81aedf0b7be0513661a7d62e45445374cec
Dan Buch <daniel.buch@gmail.com>
2010-03-31 21:50:12 -0400

    replacing tabs with spaces in a few java files

--- my-java/src/headfirst/factory/pizzas/PepperoniPizza.java
+++ my-java/src/headfirst/factory/pizzas/PepperoniPizza.java
@@ -3,12 +3,12 @@ package headfirst.factory.pizzas;
 
 public class PepperoniPizza extends Pizza {
 
-	public PepperoniPizza() {
-		name = "Pepperoni Pizza";
-		dough = "Regular Crust";
-		sauce = "Marinara Pizza Sauce";
-		toppings.add("Fresh Mozzarella");
-		toppings.add("Parmesan");
-		toppings.add("Pepperoni");
-	}
+    public PepperoniPizza() {
+        name = "Pepperoni Pizza";
+        dough = "Regular Crust";
+        sauce = "Marinara Pizza Sauce";
+        toppings.add("Fresh Mozzarella");
+        toppings.add("Parmesan");
+        toppings.add("Pepperoni");
+    }
 }
