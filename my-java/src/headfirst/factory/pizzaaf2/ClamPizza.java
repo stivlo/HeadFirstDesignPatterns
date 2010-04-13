5a311fc0cd5d2f4e89c57c767f455727bc469589
Dan Buch <daniel.buch@gmail.com>
2010-04-12 21:30:09 -0400

    adding ClamPizza for ch.4 p.151

---
+++ my-java/src/headfirst/factory/pizzaaf2/ClamPizza.java
@@ -0,0 +1,18 @@
+package headfirst.factory.pizzaaf2;
+
+
+public class ClamPizza extends Pizza {
+    PizzaIngredientFactory ingredientFactory;
+
+    public ClamPizza(PizzaIngredientFactory ingredientFactory) {
+        this.ingredientFactory = ingredientFactory;
+    }
+
+    void prepare() {
+        info("Preparing " + name);
+        dough = ingredientFactory.createDough();
+        sauce = ingredientFactory.createSauce();
+        cheese = ingredientFactory.createCheese();
+        clams = ingredientFactory.createClams();
+    }
+}
