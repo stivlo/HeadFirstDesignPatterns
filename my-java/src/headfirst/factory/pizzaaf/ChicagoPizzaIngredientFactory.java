601805b79df4a1ae1ca05cc169884043386c98c9
Dan Buch <daniel.buch@gmail.com>
2010-04-12 20:54:21 -0400

    adding Chicago ingredient factory and ingredients for ch.4 p.148

---
+++ my-java/src/headfirst/factory/pizzaaf/ChicagoPizzaIngredientFactory.java
@@ -0,0 +1,33 @@
+package headfirst.factory.pizzaaf;
+
+public class ChicagoPizzaIngredientFactory implements PizzaIngredientFactory {
+
+    public Dough createDough() {
+        return new ThickCrustDough();
+    }
+
+    public Sauce createSauce() {
+        return new PlumTomatoSauce();
+    }
+
+    public Cheese createCheese() {
+        return new Mozzarella();
+    }
+
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = {
+            new BlackOlives(),
+            new Spinach(),
+            new Eggplant(),
+        };
+        return veggies;
+    }
+
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    public Clams createClams() {
+        return new FrozenClams();
+    }
+}
